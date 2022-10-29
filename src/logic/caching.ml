8f9b57360fa51b31ebbe65352ac749dcc922d28f
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-10-29 16:12:50 -0700

    Shorter, simpler caching
    - Add cache key "compact" to avoid GitLab CI/CD failing
       on longer (100% accurate) cache keys
    - Fill missing files in cache that produced warnings

--- src/logic/caching.ml
+++ src/logic/caching.ml
@@ -1,11 +1,32 @@
 open Astring
 open Jingoo
 
-(* GitLab CI/CD constraints: https://docs.gitlab.com/ee/ci/yaml/index.html#cachekey *)
-
 let cachebust = 1
 
 let static_vars = [ ("cachebust", Jg_types.Tstr (string_of_int cachebust)) ]
+
+(* GitLab CI/CD constraints: https://docs.gitlab.com/ee/ci/yaml/index.html#cachekey *)
+let sanitize_key =
+  String.map (function
+    | '-' -> '-'
+    | c when not (Char.Ascii.is_alphanum c) -> '_'
+    | c -> c)
+
+(** The compact cache key is a tradeoff between accuracy and brevity.
+
+    In GitHub Actions we can perform a hash over a
+    long list of cache key components; that is both accurate and short.
+    
+    In GitLab CI/CD we cannot perform any expressions over the cache key
+    components. Even worse, the cache key is verbatim as a directory
+    on the Windows/Ubuntu/macOS build machine. To keep the cache key short
+    we need to find proxy values for the cache key components that may
+    not be 100% accurate but are mostly accurately.
+    
+    The expectation is that GitLab CI/CD uses the compact cache key, while
+    GitHub Actions uses the accurate cache keys. *)
+let cachekey_compact ~input:_ ~matrix ~commitref =
+  [ sanitize_key Version.dune_project_version; matrix "abi_pattern"; commitref ]
 
 let cachekey_opambin ~read_script ~input ~matrix =
   match read_script "setup-dkml.sh" with
@@ -45,7 +66,10 @@ let gh_cachekeys read_script =
   let join = String.concat ~sep:"-" in
   let input s = Printf.sprintf "${{ inputs.%s }}" s in
   let matrix s = Printf.sprintf "${{ steps.full_matrix_vars.outputs.%s }}" s in
+  let commitref = "${{ github.ref_name }}" in
   [
+    ( "gh_cachekey_compact",
+      Jg_types.Tstr (join (cachekey_compact ~input ~matrix ~commitref)) );
     ( "gh_cachekey_opambin",
       Jg_types.Tstr (join (cachekey_opambin ~read_script ~input ~matrix)) );
     ( "gh_cachekey_ci_inputs",
@@ -58,7 +82,10 @@ let gl_cachekeys read_script =
   let join = String.concat ~sep:"-" in
   let input s = Printf.sprintf "${%s}" s in
   let matrix s = Printf.sprintf "${%s}" s in
+  let commitref = "${CI_COMMIT_REF_SLUG}" in
   [
+    ( "gl_cachekey_compact",
+      Jg_types.Tstr (join (cachekey_compact ~input ~matrix ~commitref)) );
     ( "gl_cachekey_opambin",
       Jg_types.Tstr (join (cachekey_opambin ~read_script ~input ~matrix)) );
     ( "gl_cachekey_ci_inputs",
