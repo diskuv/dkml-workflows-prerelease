1330d20712230058ee79b68d589011599604a914
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2022-11-04 08:54:41 -0700

    Rework release to work with GitLab Packages

--- ci/prepare-release.sh
+++ ci/prepare-release.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -euf
 
+# Input:
+#
+# dist/
+#   <channel>/
+#      <dkml_host_abi>.tar.gz
+#
+# Output:
+# _release/
+#   <channel>.tar.gz
+
 rm -rf "_release"
 install -d "_release"
 
@@ -13,7 +23,7 @@ for CHANNEL in release next; do
         find "dist/$CHANNEL" -mindepth 1 -maxdepth 1 -type f -name "*.tar.gz" | while read -r tarball; do
             dkml_host_abi=$(basename "${tarball%.tar.gz}")
             install -d "$ARCHIVE/$dkml_host_abi"
-            tar x -vz -f "$tarball" -C "$ARCHIVE/$dkml_host_abi"
+            tar x -z -f "$tarball" -C "$ARCHIVE/$dkml_host_abi"
         done
 
         # Tar ball
