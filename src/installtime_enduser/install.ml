8587e542575695fd2c23013b7bf3633a04fae05d
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2022-11-12 16:07:05 -0800

    bugfix: support log config during install

--- src/installtime_enduser/install.ml
+++ src/installtime_enduser/install.ml
@@ -1,9 +1,5 @@
-type t = { source_dir : Fpath.t; target_dir : Fpath.t; use_dkml_fswatch : bool }
-
-let create ~source_dir ~target_dir ~use_dkml_fswatch =
-  { source_dir; target_dir; use_dkml_fswatch }
-
-let install { source_dir; target_dir; use_dkml_fswatch } =
+let install (_ : Dkml_install_api.Log_config.t) use_dkml_fswatch source_dir
+    target_dir =
   (match Diskuvbox.copy_dir ~src:source_dir ~dst:target_dir () with
   | Ok () -> ()
   | Error msg -> failwith msg);
@@ -17,34 +13,45 @@ let install { source_dir; target_dir; use_dkml_fswatch } =
     | Ok () -> ()
     | Error msg -> failwith msg
 
-let cli () =
-  (* Arg parsing *)
-  let anon_fun (_ : string) =
-    failwith "No commandline arguments supported for ocamlrun install.ml"
+let use_dkml_fswatch_t =
+  let doc =
+    "If enabled, copy bin/dkml-fswatch.exe from the source into \
+     tools/fswatch/fswatch of the destination"
   in
-  let source_dir = ref "" in
-  let target_dir = ref "" in
-  let use_dkml_fswatch = ref false in
-  Arg.(
-    parse
-      [
-        ("--source-dir", Set_string source_dir, "Source path");
-        ("--target-dir", Set_string target_dir, "Destination path");
-        ( "--use-dkml-fswatch",
-          Set use_dkml_fswatch,
-          "If enabled, copy bin/dkml-fswatch.exe from the source into \
-           tools/fswatch/fswatch of the destination" );
-      ]
-      anon_fun "Install the desktop binaries in --source-dir into --target-dir");
-  if !source_dir = "" then (
-    prerr_endline "FATAL: The --source-dir DIR option is required.";
-    exit 1);
-  if !target_dir = "" then (
-    prerr_endline "FATAL: The --target-dir DIR option is required.";
-    exit 1);
+  Cmdliner.Arg.(value & flag & info ~doc [ "use-dkml-fswatch" ])
 
-  let installer =
-    create ~source_dir:(Fpath.v !source_dir) ~target_dir:(Fpath.v !target_dir)
-      ~use_dkml_fswatch:!use_dkml_fswatch
+let source_dir_t =
+  let x =
+    Cmdliner.Arg.(
+      required & opt (some dir) None & info ~doc:"Source path" [ "source-dir" ])
   in
-  install installer
+  Cmdliner.Term.(const Fpath.v $ x)
+
+let target_dir_t =
+  let x =
+    Cmdliner.Arg.(
+      required
+      & opt (some string) None
+      & info ~doc:"Target path" [ "target-dir" ])
+  in
+  Cmdliner.Term.(const Fpath.v $ x)
+
+let setup_log style_renderer level =
+  Fmt_tty.setup_std_outputs ?style_renderer ();
+  Logs.set_level level;
+  Logs.set_reporter (Logs_fmt.reporter ());
+  Dkml_install_api.Log_config.create ?log_config_style_renderer:style_renderer
+    ?log_config_level:level ()
+
+let setup_log_t =
+  Cmdliner.Term.(
+    const setup_log $ Fmt_cli.style_renderer () $ Logs_cli.level ())
+
+let cli () =
+  let t =
+    Cmdliner.Term.
+      ( const install $ setup_log_t $ use_dkml_fswatch_t $ source_dir_t
+        $ target_dir_t,
+        info "opam-install.bc" ~doc:"Install opam" )
+  in
+  Cmdliner.Term.(exit @@ eval t)
