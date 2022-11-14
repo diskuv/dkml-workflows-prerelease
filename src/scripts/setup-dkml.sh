70b30197b3a9b33e484bfe929a59fe7cbc9e7dfc
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-11-13 17:04:52 -0800

    Bump diskuv-opam-repository and dkml-apps

--- src/scripts/setup-dkml.sh
+++ src/scripts/setup-dkml.sh
@@ -4,7 +4,7 @@ set -euf
 # Constants
 SHA512_DEVNULL='cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e'
 #   Edited by https://gitlab.com/diskuv/diskuv-ocaml/contributors/release.sh
-DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=0857031f4c11473f52e2deba72436694a9e57450
+DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=6d2b7c821f2f306cf0d6c2e61d3e8b84f3e65cf3
 # Constants
 #   Should be edited by release.sh, but ...
 #   Can't be 1.0.0 or later until https://github.com/ocaml/opam-repository/pull/21704 ocaml-option-32bit
@@ -83,7 +83,7 @@ VERBOSE=${VERBOSE:-}
 Generated Constants
 -------------------
 DKML_VERSION=$DKML_VERSION
-DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=0857031f4c11473f52e2deba72436694a9e57450$DEFAULT_DISKUV_OPAM_REPOSITORY_TAG
+DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=6d2b7c821f2f306cf0d6c2e61d3e8b84f3e65cf3$DEFAULT_DISKUV_OPAM_REPOSITORY_TAG
 DEFAULT_DKML_COMPILER=$DEFAULT_DKML_COMPILER
 .
 ------
