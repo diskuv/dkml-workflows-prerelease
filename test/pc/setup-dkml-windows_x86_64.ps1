2905eb7d2b7406088e74c512ccdde520b9381817
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-11-18 11:50:52 -0800

    promote runtest generated code

--- test/pc/setup-dkml-windows_x86_64.ps1
+++ test/pc/setup-dkml-windows_x86_64.ps1
@@ -135,16 +135,16 @@ param (
   ,[Parameter()] [string] $PIN_CTYPES = "0.19.2-windowssupport-r4"
   ,[Parameter()] [string] $PIN_CURLY = "0.2.1-windows-env_r2"
   ,[Parameter()] [string] $PIN_DIGESTIF = "1.0.1"
-  ,[Parameter()] [string] $PIN_DUNE = "2.9.3+shim.1.0.2~r0"
+  ,[Parameter()] [string] $PIN_DUNE = "2.9.3+shim.1.0.2~r8"
   ,[Parameter()] [string] $PIN_DUNE_CONFIGURATOR = "2.9.3"
-  ,[Parameter()] [string] $PIN_DKML_APPS = "1.0.2~prerel8"
+  ,[Parameter()] [string] $PIN_DKML_APPS = "1.0.2~prerel9"
   ,[Parameter()] [string] $PIN_OCAMLBUILD = "0.14.0"
   ,[Parameter()] [string] $PIN_OCAMLFIND = "1.9.1"
   ,[Parameter()] [string] $PIN_OCP_INDENT = "1.8.2-windowssupport"
   ,[Parameter()] [string] $PIN_PPX_EXPECT = "v0.14.1"
   ,[Parameter()] [string] $PIN_PTIME = "0.8.6-msvcsupport"
   ,[Parameter()] [string] $PIN_TIME_NOW = "v0.14.0"
-  ,[Parameter()] [string] $PIN_WITH_DKML = "1.0.2~prerel8"
+  ,[Parameter()] [string] $PIN_WITH_DKML = "1.0.2~prerel9"
 )
 
 $ErrorActionPreference = "Stop"
@@ -506,7 +506,7 @@ set -euf
 # Constants
 SHA512_DEVNULL='cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e'
 #   Edited by https://gitlab.com/diskuv/diskuv-ocaml/contributors/release.sh
-DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=26dfc8a980d30023a05c73e6bd38425ae34488d5
+DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=8696ca55b11bb7a3a9f1cd46ca53e55bc12cc5ca
 # Constants
 #   Should be edited by release.sh, but ...
 #   Can't be 1.0.0 or later until https://github.com/ocaml/opam-repository/pull/21704 ocaml-option-32bit
@@ -1774,5 +1774,5 @@ Now you can use 'opamrun' to do opam commands like:
 
   msys64\usr\bin\bash -lc 'PATH="`$PWD/.ci/sd4/opamrun:`$PATH"; opamrun install XYZ.opam'
   msys64\usr\bin\bash -lc 'PATH="`$PWD/.ci/sd4/opamrun:`$PATH"; opamrun -it exec -- bash'
-  msys64\usr\bin\bash -lc 'PATH="`$PWD/.ci/sd4/opamrun:`$PATH"; opamrun exec -- sh ci/build-test.sh'
+  msys64\usr\bin\bash -lc 'PATH="sh ci/build-test.sh'
 "@
