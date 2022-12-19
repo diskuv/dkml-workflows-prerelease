ce204fd7c0d051aa2936645e5be9cee4fda76c1a
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-12-19 02:53:39 -0800

    Bump diskuv-opam-repository and dkml-apps

--- src/scripts/setup-dkml.sh
+++ src/scripts/setup-dkml.sh
@@ -4,7 +4,7 @@ set -euf
 # Constants
 SHA512_DEVNULL='cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e'
 #   Edited by https://gitlab.com/diskuv/diskuv-ocaml/contributors/release.sh
-DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=9f37e71eef1500242a4da1f434b628a989ff8900
+DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=c8bdf9c88f8630086ff504ec1d6a83c69c0a8346
 # Constants
 #   Should be edited by release.sh, but ...
 #   Can't be 1.0.0 or later until https://github.com/ocaml/opam-repository/pull/21704 ocaml-option-32bit
