b079de3a85c8ab2201bfb2c8f6de9429f13e53eb
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-12-30 00:49:45 -0800

    Tests with DKML 1.1.0

--- test/pc/setup-dkml-darwin_x86_64.sh
+++ test/pc/setup-dkml-darwin_x86_64.sh
@@ -28,14 +28,14 @@ export PIN_CTYPES='0.19.2-windowssupport-r4'
 export PIN_CURLY='0.2.1-windows-env_r2'
 export PIN_DIGESTIF='1.0.1'
 export PIN_DUNE='3.6.2'
-export PIN_DKML_APPS='1.1.0~prerel27'
+export PIN_DKML_APPS='1.1.0'
 export PIN_OCAMLBUILD='0.14.0'
 export PIN_OCAMLFIND='1.9.1'
 export PIN_OCP_INDENT='1.8.2-windowssupport'
 export PIN_PPX_EXPECT='v0.14.1'
 export PIN_PTIME='0.8.6-msvcsupport'
 export PIN_TIME_NOW='v0.14.0'
-export PIN_WITH_DKML='1.1.0~prerel27'
+export PIN_WITH_DKML='1.1.0'
 
 usage() {
   echo 'Setup Diskuv OCaml (DKML) compiler on a desktop PC.' >&2
@@ -407,9 +407,9 @@ set -euf
 # Constants
 SHA512_DEVNULL='cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e'
 #   Edited by https://gitlab.com/diskuv/diskuv-ocaml/contributors/release.sh
-DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=6303854b20a2bfa1edf7df995a60a3777f46f9fa
+DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=cdf6463c5c3c7ca37bc798e17929bff1c481bfdd
 # Constants
-DKML_VERSION=1.1.0-prerel27
+DKML_VERSION=1.1.0
 
 setup_WORKSPACE_VARNAME=$1
 shift
