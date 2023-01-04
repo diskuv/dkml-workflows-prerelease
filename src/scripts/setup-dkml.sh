521bc5d1c57153dbaeb949946e3d708e4d70826c
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-01-04 11:33:19 -0800

    Bump diskuv-opam-repository and dkml-apps

--- src/scripts/setup-dkml.sh
+++ src/scripts/setup-dkml.sh
@@ -4,9 +4,9 @@ set -euf
 # Constants
 SHA512_DEVNULL='cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e'
 #   Edited by https://gitlab.com/diskuv/diskuv-ocaml/contributors/release.sh
-DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=cdf6463c5c3c7ca37bc798e17929bff1c481bfdd
+DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=8bfc350adddfa323afe05b37054aaf2aa55f8808
 # Constants
-DKML_VERSION=1.1.0
+DKML_VERSION=1.2.0-prerel1
 
 setup_WORKSPACE_VARNAME=$1
 shift
