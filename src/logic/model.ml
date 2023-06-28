dcc855a3b87077b4c372ad2da4ba018cd79228eb
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-06-27 20:02:17 -0700

    Version: 1.2.1-10

--- src/logic/model.ml
+++ src/logic/model.ml
@@ -10,7 +10,7 @@ open Jingoo
 
 let global_env_vars =
   [
-    ("DEFAULT_DKML_COMPILER", "4.14.0-v1.2.1-3");
+    ("DEFAULT_DKML_COMPILER", "4.14.0-v1.2.1-10");
 
     (* BEGIN pin-env-vars. DO NOT EDIT THE LINES IN THIS SECTION *)
     (* Managed by bump-packages.cmake *)
