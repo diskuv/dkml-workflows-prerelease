88eceb66f1029888a070c4eb7458a83cbf0a1119
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-06-30 22:33:31 -0700

    Rename DKML_RELEASE_PARTICIPANT_MODULE to DKML_BUMP_VERSION_PARTICIPANT_MODULE

--- bump-version.cmake
+++ bump-version.cmake
@@ -1,10 +1,10 @@
 # Test with:
-# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-workflows -D "regex_DKML_VERSION_SEMVER=1[.]2[.]1-[0-9]+" -D "regex_DKML_VERSION_OPAMVER=1[.]2[.]1[~]prerel[0-9]+" -D DKML_VERSION_SEMVER_NEW=1.2.1-3 -D DKML_VERSION_OPAMVER_NEW=1.2.1~prerel3 -D GIT_EXECUTABLE=git -D DKML_RELEASE_OCAML_VERSION=4.14.0 -D DKML_RELEASE_PARTICIPANT_MODULE=../../../packaging/version-bump/DkMLBumpVersionParticipant.cmake -P bump-version.cmake
+# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-workflows -D "regex_DKML_VERSION_SEMVER=1[.]2[.]1-[0-9]+" -D "regex_DKML_VERSION_OPAMVER=1[.]2[.]1[~]prerel[0-9]+" -D DKML_VERSION_SEMVER_NEW=1.2.1-3 -D DKML_VERSION_OPAMVER_NEW=1.2.1~prerel3 -D GIT_EXECUTABLE=git -D DKML_RELEASE_OCAML_VERSION=4.14.0 -D DKML_BUMP_VERSION_PARTICIPANT_MODULE=../../../packaging/version-bump/DkMLBumpVersionParticipant.cmake -P bump-version.cmake
 
-if(NOT DKML_RELEASE_PARTICIPANT_MODULE)
-    message(FATAL_ERROR "Missing -D DKML_RELEASE_PARTICIPANT_MODULE=.../DkMLBumpVersionParticipant.cmake")
+if(NOT DKML_BUMP_VERSION_PARTICIPANT_MODULE)
+    message(FATAL_ERROR "Missing -D DKML_BUMP_VERSION_PARTICIPANT_MODULE=.../DkMLBumpVersionParticipant.cmake")
 endif()
-include(${DKML_RELEASE_PARTICIPANT_MODULE})
+include(${DKML_BUMP_VERSION_PARTICIPANT_MODULE})
 
 DkMLBumpVersionParticipant_ModelReplace(src/logic/model.ml)
 DkMLBumpVersionParticipant_DuneProjectReplace(dune-project)
