9d7deaabdef557b174c6324cc6f12728db0cad5c
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-07-05 08:35:22 -0700

    Use DkMLBumpPackagesParticipant_TestPromote
    The test/** code needs to be updated each
    DkML release

--- bump-packages.cmake
+++ bump-packages.cmake
@@ -7,8 +7,22 @@
 if(NOT DKML_BUMP_PACKAGES_PARTICIPANT_MODULE)
     message(FATAL_ERROR "Missing -D DKML_BUMP_PACKAGES_PARTICIPANT_MODULE=.../DkMLBumpPackagesParticipant.cmake")
 endif()
+
 include(${DKML_BUMP_PACKAGES_PARTICIPANT_MODULE})
 
 DkMLBumpPackagesParticipant_SetupDkmlUpgrade(src/scripts/setup-dkml.sh)
 DkMLBumpPackagesParticipant_ModelUpgrade(src/logic/model.ml)
+DkMLBumpPackagesParticipant_TestPromote(REL_FILENAMES
+    test/gh-darwin/post/action.yml
+    test/gh-darwin/pre/action.yml
+    test/gh-linux/post/action.yml
+    test/gh-linux/pre/action.yml
+    test/gh-windows/post/action.yml
+    test/gh-windows/pre/action.yml
+    test/gl/setup-dkml.gitlab-ci.yml
+    test/pc/setup-dkml-darwin_x86_64.sh
+    test/pc/setup-dkml-linux_x86.sh
+    test/pc/setup-dkml-linux_x86_64.sh
+    test/pc/setup-dkml-windows_x86.ps1
+    test/pc/setup-dkml-windows_x86_64.ps1)
 DkMLBumpPackagesParticipant_GitAddAndCommit()
