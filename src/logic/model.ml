94113cc2d2cf32d1786db0767a0482370fbe146e
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-08-28 21:00:01 -0700

    Version: 2.0.3

--- src/logic/model.ml
+++ src/logic/model.ml
@@ -11,9 +11,9 @@ open Jingoo
 let global_env_vars =
   [
     (* Edited by bump-version.cmake *)
-    ("DKML_VERSION", "2.0.2");
-    ("DEFAULT_DISKUV_OPAM_REPOSITORY_TAG", "2.0.2");
-    ("DEFAULT_DKML_COMPILER", "2.0.2");
+    ("DKML_VERSION", "2.0.3");
+    ("DEFAULT_DISKUV_OPAM_REPOSITORY_TAG", "2.0.3");
+    ("DEFAULT_DKML_COMPILER", "2.0.3");
 
     (* BEGIN pin-env-vars. DO NOT EDIT THE LINES IN THIS SECTION *)
     (* Managed by bump-packages.cmake *)
