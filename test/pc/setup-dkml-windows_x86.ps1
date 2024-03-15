33068ea2089ae1d8ea48a9e19cbedfc6a327c197
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2024-03-14 20:40:15 -0700

    Spread packages over 5 variations (2/n)

--- test/pc/setup-dkml-windows_x86.ps1
+++ test/pc/setup-dkml-windows_x86.ps1
@@ -1357,7 +1357,7 @@ if [ "${in_docker:-}" = "true" ] && [ -n "${dockcross_image:-}" ]; then
     install -d .ci/sd4/docker-image
     #   Since GitLab CI limits environment variables to 255 characters, if you need to exceed that there are five (5)
     #   variations of `dockcross_packages_apt` and `dockcross_packages_yum` to spread the packages over.
-    printf "FROM %s\nENV DEFAULT_DOCKCROSS_IMAGE %sdkml-workflows/dockcross:latest\nRUN if command -v apt-get; then apt-get install -y rsync %s && rm -rf /var/lib/apt/lists/*; fi\nRUN if command -v yum; then yum install -y rsync %s && yum clean all && rm -rf /var/cache/yum; fi" \
+    printf "FROM %s\nENV DEFAULT_DOCKCROSS_IMAGE %sdkml-workflows/dockcross:latest\nRUN if command -v apt-get; then apt-get install -y rsync %s %s %s %s %s && rm -rf /var/lib/apt/lists/*; fi\nRUN if command -v yum; then yum install -y rsync %s %s %s %s %s && yum clean all && rm -rf /var/cache/yum; fi" \
         "${dockcross_image:-}" "${docker_fqin_preusername}" \
         "${dockcross_packages_apt:-}" "${dockcross_packages_apt2:-}" "${dockcross_packages_apt3:-}" "${dockcross_packages_apt4:-}" "${dockcross_packages_apt5:-}" \
         "${dockcross_packages_yum:-}" "${dockcross_packages_yum2:-}" "${dockcross_packages_yum3:-}" "${dockcross_packages_yum4:-}" "${dockcross_packages_yum5:-}" \
